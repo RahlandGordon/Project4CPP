bac6e7e66eb717855e5613866bdc6b90166b093d
RahlandGordon <rahland.gordon.12@cnu.edu>
2015-10-24 13:10:22 -0400

    Update StringParserClass.cpp

--- StringParser/StringParserClass.cpp
+++ StringParser/StringParserClass.cpp
@@ -9,3 +9,35 @@ using namespace std;
 using namespace KP_StringParserClass;
 
 //TODO Fill this in
+StringParserClass::StringParserClass(void){
+		char	*pStartTag = new char;
+		char	*pEndTag = new char;
+		bool	areTagsSet = false;
+		int		lastError=0;
+}
+StringParserClass::~StringParserClass(){}
+int StringParserClass::getLastError(){
+
+	return lastError;
+}
+bool StringParserClass::setTags(const char *pStartTag, const char *pEndTag){
+	
+				//length of start and endtag
+				int sl= strlen(pStartTag)++;
+				int el = strlen(pEndTag)++;
+
+				//initialize mem space of pStartTag, pEndTag
+				StringParserClass::pStartMem = (char*)malloc(sl);
+				StringParserClass::pEndMem = (char*)malloc(el);
+
+				//set tag to pStartTag
+				strncpy(StringParserClass::pStartMem,pStartMem,sl);
+				strncpy(StringParserClass::pEndMem,pEndMem,el);
+				if(*StringParserClass::pStartMem != NULL && *StringParserClass::pEndMem != NULL){
+					areTagsSet = true;
+				}else{
+					lastError = ERROR_DATA_NULL;
+					return false;
+				}
+				return areTagsSet;
+		}
